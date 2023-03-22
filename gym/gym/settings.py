ff3aef589bb39ccf11d9ab0663279b61d6f4ab53
boguszmedykowski <bogusz.medykowski@gmail.com>
2023-03-22 20:05:05 +0100

    install swagger

--- gym/gym/settings.py
+++ gym/gym/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'note'
+    'drf_spectacular',
+    'note',
 ]
 
 MIDDLEWARE = [
@@ -111,3 +112,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- gym/note/urls.py
+++ gym/note/urls.py
@@ -3,9 +3,8 @@ from rest_framework import routers
 from .views import WorkoutViewSet
 
 router = routers.DefaultRouter()
-router.register(r'users', WorkoutViewSet)
+router.register(r'workout', WorkoutViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
