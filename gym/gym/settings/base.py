0fa38f0f4e501cf328240cdfd8e72c12028167f1
boguszmedykowski <bogusz.medykowski@gmail.com>
2023-03-25 22:00:10 +0100

    python-dotenv

--- gym/gym/settings/base.py
+++ gym/gym/settings/base.py
@@ -1,24 +1,18 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6lyh0bs3_=&2piauy4qslfapmxxy(ji(xki1_hq*p7uqlnq_vy'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = int(os.environ.get('DEBUG', default=1))
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
--- gym/manage.py
+++ gym/manage.py
@@ -2,11 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from gym.settings import base
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gym.settings')
+
+    if base.DEBUG == True:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gym.settings.local')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gym.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
