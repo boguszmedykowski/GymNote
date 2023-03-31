a179ebe1d3725aab7ca8c0b4bee4f40ebd606e20
bogusznowy@gmail.com <bogusz.medykowski@3mk.pl>
2023-03-31 14:22:17 +0200

    test api users

--- gym/users/views.py
+++ gym/users/views.py
@@ -1,4 +1,8 @@
-from rest_framework import generics
+from rest_framework import (
+    generics,
+    authentication,
+    permissions
+)
 from users.serializers import (
     AuthTokenSerializer,
     UserSerializer,
@@ -16,4 +20,12 @@ class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and retun the authenticated user"""
+        return self.request.user
 
