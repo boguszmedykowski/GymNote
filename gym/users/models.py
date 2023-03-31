157a0bd12fc7af0a0f7848ebcf770ea8e87c1fa8
bogusznowy@gmail.com <bogusz.medykowski@3mk.pl>
2023-03-31 08:41:00 +0200

    fix createsuperuser

--- gym/users/models.py
+++ gym/users/models.py
@@ -7,20 +7,24 @@ from django.contrib.auth.models import (
 # from django.contrib.auth import get_user_model
 # User = get_user_model()
 
+
 class UserManager(BaseUserManager):
-
-    def create_user(self, username, email, password=None, **kwargs):
-        user = self.model(username=username, email=email, **kwargs)
+    def create_user(self, email, password=None, **kwargs):
+        user = self.model(email=self.normalize_email(email), **kwargs)
         user.set_password(password)
-        user.save(using=self)
-
+        user.save(using=self._db)
         return user
+
+    def create_superuser(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+        return self.create_user(email, password, **kwargs)
 
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=255, unique= True)
-    username = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
